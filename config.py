52bda9675e968d9f68ccf63d919038077fb6feb3
Rafael Lima <rafael.liverpool@gmail.com>
2016-04-11 20:46:10 -0300

    [IMP] Terminal utilizando pseudo tty

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ CSRF_ENABLED = True
 APP_NAME = "Orka"
 
 # Uncomment to setup Setup an App icon
-APP_ICON = "/static/img/orka.png"
+APP_ICON = "/static/img/whale.png"
 
 #----------------------------------------------------
 # AUTHENTICATION CONFIG
@@ -104,12 +104,12 @@ MAIL_DEFAULT_SENDER = 'orka-95f195@inbox.mailtrap.io'
 #APP_THEME = "amelia.css"
 #APP_THEME = "cosmo.css"
 #APP_THEME = "cyborg.css"  
-APP_THEME = "flatly.css"
+#APP_THEME = "flatly.css"
 #APP_THEME = "journal.css"
 #APP_THEME = "readable.css"
 #APP_THEME = "simplex.css"
 #APP_THEME = "slate.css"   
-#APP_THEME = "spacelab.css"
+APP_THEME = "spacelab.css"
 #APP_THEME = "united.css"
 #APP_THEME = "yeti.css"
 
