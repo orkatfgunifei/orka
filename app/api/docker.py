7565900cf0344ebc91a45debfca59ea1ac13b833
Rafael Lima <rafael.liverpool@gmail.com>
2016-09-29 02:06:53 -0300

    Better better api

--- app/api/docker.py
+++ app/api/docker.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 from subprocess import Popen, PIPE
-
+import json
 
 """
     API Docker - Python
@@ -10,25 +10,84 @@ from subprocess import Popen, PIPE
 class Docker():
 
     def run(self, name):
-        self.execute('docker run ' + name)
+        """
+        Executa uma imagem em um novo container
+        Caso já exista ele retorna o nome do container
+        :param name: Nome da imagem requisitada
+        :return: (True ou False, hash_nome ou nome) --> Tupla de retorno
+        """
+        resp = self.execute('docker run -d ' + name)
+
+        if resp != "":
+            return  True, resp
+        else:
+            cmd = name.split().index('--name') + 1
+            name = name.split()[cmd]
+            print "[INFO] Container com nome: " + name + " já existente, modifique o nome.\n"
+            return False, name
+
+
+    def inspect(self, name):
+        """
+        Inspeciona um container
+        :param name: nome do container
+        :return: Retorna um dicionário com todas as informações do container
+        """
+        resp_json = self.execute('docker inspect ' + name)
+        if resp_json:
+            resp = json.loads(resp_json)[0]
+            return resp
+        else:
+            return False
+
+    def attach(self, name):
+        # TODO: Criar Tratamento de threads !
+        resp = self.execute('docker attach ' + name)
+        print resp
 
     def rm(self, name):
-        self.kill_and_remove(name)
+        """
+        Remoção de container
+        :param name: nome ou hash do container
+        :return: True se tudo ocorrer bem
+        """
+        return self.kill_and_remove(name)
 
     def start(self, name):
-        self.execute('docker start ' + name)
+        """
+        Inícia container
+        :param name: nome do container
+        :return: nome ou hash do container
+        """
+        return self.execute('docker start ' + name)
 
     def stop(self, name):
-        self.execute('docker stop ' + name)
+        """
+        Encerra container
+        :param name: nome do container
+        :return: nome ou hash do container
+        """
+        return self.execute('docker stop ' + name)
 
-    def list(self, all=False):
+    def list_containers(self, all=False):
+        """
+        Exibe no console a lista de containeres
+        :param all: False para exibir apenas containeres ativos, e True para exibir todos
+        :return: string com a lista de containeres
+        """
         command = "docker ps"
 
         if all:
             command += " -a"
 
-        self.execute(command)
+        return self.execute(command)
 
+    def list_images(self):
+        """
+        Exibe as imagens na máquina
+        :return: string com as informações das imagens disponíveis
+        """
+        return self.execute('docker images')
 
     def execute(self, command):
         """
@@ -53,11 +112,24 @@ class Docker():
             if p.wait() != 0:
                 raise RuntimeError(p.stderr.read())
 
+        return True
+
 
     def __repr__(self):
-        return self.execute("docker ps -a")
+        return self.execute("docker version")
 
 
-if __name__ == '__main__':
-    a = Docker()
-    print a
+# if __name__ == '__main__':
+#     a = Docker()
+#     print a
+#     comando = "-p 3000:3000 --name some-redmine redmine"
+#     imagem = a.run(comando)
+#     print a.list_containers()
+#
+#     #a.rm(imagem[1])
+#
+#     print a.list_containers()
+#
+#     #Necessário uso de thread para attach !
+#     # if imagem:
+#     #     a.attach(imagem[:8])
