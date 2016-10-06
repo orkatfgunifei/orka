041bab3c9940b49e00f6a9b922514c5bf6b375ab
Rafael Lima <rafael.liverpool@gmail.com>
2016-10-06 17:56:06 -0300

    pop

--- populate.py
+++ populate.py
@@ -1,5 +1,5 @@
 from app import db
-from app.models import ContactGroup, Gender, Contact
+from app.models.contact import ContactGroup, Gender, Contact
 import random
 from datetime import datetime
 
@@ -10,16 +10,16 @@ def get_random_name(names_list, size=1):
 
 
 try:
-    db.session.add(ContactGroup(name='Friends'))
-    db.session.add(ContactGroup(name='Family'))
-    db.session.add(ContactGroup(name='Work'))
+    db.session.add(ContactGroup(name='DEV Time'))
+    db.session.add(ContactGroup(name='Clientes'))
+    db.session.add(ContactGroup(name='Amigos'))
     db.session.commit()
 except:
     db.session.rollback()
 
 try:
-    db.session.add(Gender(name='Male'))
-    db.session.add(Gender(name='Female'))
+    db.session.add(Gender(name='Masculino'))
+    db.session.add(Gender(name='Feminino'))
     db.session.commit()
 except:
     db.session.rollback()
@@ -32,7 +32,7 @@ f.close()
 for i in range(1, 50):
     c = Contact()
     c.name = get_random_name(names_list, random.randrange(2, 6))
-    c.address = 'Street ' + names_list[random.randrange(0, len(names_list))]
+    c.address = 'Rua ' + names_list[random.randrange(0, len(names_list))]
     c.personal_phone = random.randrange(1111111, 9999999)
     c.personal_celphone = random.randrange(1111111, 9999999)
     c.contact_group_id = random.randrange(1, 4)
@@ -44,7 +44,7 @@ for i in range(1, 50):
     db.session.add(c)
     try:
         db.session.commit()
-        print "inserted", c
+        print "inserido", c
     except:
         db.session.rollback()
     
