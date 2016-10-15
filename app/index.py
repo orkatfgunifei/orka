612b16131ba2d3890e8379e4988ed04ed2de0ffb
Diogo Matos <alvesdiogo.mts@gmail.com>
2016-10-15 02:24:48 -0300

    [DEV] exemplo requisicao consumo maquina

--- app/index.py
+++ app/index.py
@@ -1,9 +1,19 @@
-from flask.ext.appbuilder import IndexView
+#coding: utf-8
+import flask
+from flask.ext.appbuilder import IndexView, has_access, expose
+from flask import g, redirect, url_for, request, redirect, make_response, session
 
 class IndexView(IndexView):
     index_template = 'index.html'
 
+    # TODO: Informações do sistema para o json, requisição se repete a cada 2,5 segundos no FrontEnd##
+    @expose('/usage', methods=['GET'])
+    @has_access
+    def dashboard(self):
+        self.update_redirect()
+        json = {'chave': 'valor'}
 
+        return flask.jsonify(json)
 
 
 
