042306238764af8737dfb817bbc82743df1d77b1
Rafael Lima <rafael.liverpool@gmail.com>
2016-11-09 20:40:42 -0200

    [NEW] Pesquisar funcionando

--- app/index.py
+++ app/index.py
@@ -61,6 +61,67 @@ class IndexView(IndexView):
             return self.render_template('orka/landing.html',
                                         appbuilder=self.appbuilder)
 
+    @expose('/search', methods=['POST'])
+    @has_access
+    def search(self):
+
+        data = request.form["q"]
+
+        if not data:
+            containers = self.appbuilder.session.query(Container).filter_by(created_by=g.user).all()
+        else:
+            containers = self.appbuilder.session.query(Container).filter_by(name=data).all()
+
+        for container in containers:
+
+            try:
+
+                info_container = inspect_container(container.hash_id)
+
+                status = info_container.get('State')
+
+                if not status['Running'] and container.status:
+                    # containers.pop(index_container)
+                    container.status = False
+                elif status['Running'] and not container.status:
+                    container.status = True
+
+            except:
+                container.status = False
+
+        if not data:
+            services = self.appbuilder.session.query(Service).filter_by(created_by=g.user).all()
+        else:
+            services = self.appbuilder.session.query(Service).filter_by(name=data).all()
+
+        for service in services:
+
+            try:
+                info_service = inspect_service(service.service_id)
+
+                if info_service and not service.status:
+                    service.status = True
+            except:
+                service.status = False
+
+        if self.appbuilder.session.dirty:
+            self.appbuilder.session.commit()
+
+        if not data:
+            nodes = self.appbuilder.session.query(Node).all()
+        else:
+            nodes = self.appbuilder.session.query(Node).filter_by(name=data).all()
+
+        self.update_redirect()
+
+        return self.render_template(self.index_template,
+                                    appbuilder=self.appbuilder,
+                                    containers=containers,
+                                    services=services,
+                                    nodes=nodes
+                                    )
+
+
     # TODO: Informações do sistema para o json, requisição se repete a cada 2,5 segundos no FrontEnd##
     @expose('/usage', methods=['GET'])
     @has_access
